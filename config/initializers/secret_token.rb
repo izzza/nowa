22fe2e9de0acb7da77bc75f9acf00a23d5dc823f
izzza <izabela.kulakowska@ananastar.com>
2012-07-12 11:37:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nowa::Application.config.secret_token = '611ddff2b5fa004a66f4e052b3beee4926c9e68c9d9a2e59e272ca8028300fa11a8ad0b51e0473646633f8e923cd3259259db7985a8904c49379d66c8533973e'
